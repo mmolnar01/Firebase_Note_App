0b78f37103123fb86bdd5a65203c39c44953a83b
Molnár Márton <molnar.marton.hun@gmail.com>
2024-02-25 11:26:03 +0100

    Splash screen

---
+++ NoteApp/app/src/main/java/hu/klm60o/android/noteapp/CreateAccountActivity.kt
@@ -0,0 +1,11 @@
+package hu.klm60o.android.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
---
+++ NoteApp/app/src/main/java/hu/klm60o/android/noteapp/SplashActivity.kt
@@ -0,0 +1,21 @@
+package hu.klm60o.android.noteapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    val handler = Handler(Looper.getMainLooper())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
+    }
+}
