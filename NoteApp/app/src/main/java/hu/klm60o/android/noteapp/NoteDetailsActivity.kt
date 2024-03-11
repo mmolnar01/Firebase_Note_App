85a4dfb6de9ba761235ab525c2c80f28af62fb30
Molnár Márton <molnar.marton.hun@gmail.com>
2024-03-11 11:56:56 +0100

    Note Details Screen added

---
+++ NoteApp/app/src/main/java/hu/klm60o/android/noteapp/NoteDetailsActivity.kt
@@ -0,0 +1,11 @@
+package hu.klm60o.android.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_details)
+    }
+}
