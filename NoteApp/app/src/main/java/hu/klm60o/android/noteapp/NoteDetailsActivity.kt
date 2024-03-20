6265cfda27299679c81119a4e1dbe774ad709ae3
Molnár Márton <molnar.marton.hun@gmail.com>
2024-03-20 13:40:46 +0100

    Add New Note Screen

--- NoteApp/app/src/main/java/hu/klm60o/android/noteapp/NoteDetailsActivity.kt
+++ NoteApp/app/src/main/java/hu/klm60o/android/noteapp/NoteDetailsActivity.kt
@@ -2,10 +2,30 @@ package hu.klm60o.android.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.klm60o.android.noteapp.databinding.ActivityMainBinding
+import hu.klm60o.android.noteapp.databinding.ActivityNoteDetailsBinding
 
 class NoteDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNoteDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_details)
+        binding = ActivityNoteDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addNoteSaveButton.setOnClickListener {
+            saveNote()
+        }
+
+    }
+
+    private fun saveNote() {
+        var title = binding.addNoteTitle.text.toString()
+        var text = binding.addNoteText.text.toString()
+
+        if (title.isEmpty()) {
+            binding.addNoteTitle.setError("Title required!")
+            return
+        }
+        return
     }
 }
